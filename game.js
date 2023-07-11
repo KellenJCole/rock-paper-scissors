ad89a231e16c3deeb98e2c7f87de3c0d99ef99e2
Kellen Cole <kellencole03@gmail.com>
2023-07-11 13:11:19 -0700

    Refactor game logic for UI

--- game.js
+++ game.js
@@ -1,75 +1,88 @@
+let playerScore = 0;
+let computerScore = 0;
+let playerChoice;
+let gameOver = false;
 
-function getPlayerChoice() {
-    let choice = prompt("Enter rock, paper, or scissors");
-    choice = choice.toLowerCase();
-    choice = choice.trim();
-    return choice;
+let cards = document.querySelectorAll('.card');
+console.log(cards);
+
+cards.forEach(function(card) {
+    card.addEventListener('mouseenter', hoverEffect);
+    card.addEventListener('mouseleave', hoverEffect);
+    card.addEventListener('click', playerSelection);
+})
+
+function hoverEffect(e) {
+    if (e.type === 'mouseenter') {
+        e.target.style.backgroundColor = "#dddddd";
+    } else if (e.type === 'mouseleave') {
+        e.target.style.backgroundColor = "white";
+    } else {
+        console.log("error");
+    }
 }
 
-function getComputerChoice() {
-    return Math.floor(Math.random() * 3) + 1; // Returns 1, 2, or 3, representing rock, paper, or scissors
-}
-
-function playRound(compChoice, playerChoice) {
-    let rpsInt;
-    switch (playerChoice) {
-        case "rock":
-            rpsInt = 1;
-            break;
-        case "paper":
-            rpsInt = 2;
-            break;
-        case "scissors":
-            rpsInt = 3;
-            break;
-        default:
-            console.log("Invalid player selection");
-            break;
+function playerSelection(e) {
+    if (e.target.getAttribute('id') === "rock" || e.target.parentNode.getAttribute('id') === "rock") {
+        playerChoice = 1;
+    } else if (e.target.id == "paper" || e.target.parentNode.getAttribute('id') === "paper") {
+        playerChoice  = 2;
+    } else if (e.target.id == "scissors" || e.target.parentNode.getAttribute('id') === "scissors") {
+        playerChoice = 3;
     }
 
-    let returnString;
-
-    if (rpsInt === compChoice) {
-        returnString = `You tied, both you and the computer played ${playerChoice}`;
-    } else if (rpsInt === 1 && compChoice === 2) {
-        returnString = "You lost! Computer's paper beats your rock";
-    } else if (rpsInt === 1 && compChoice === 3) {
-        returnString = "You won! Your rock beat the computer's scissors";
-    } else if (rpsInt === 2 && compChoice === 1) {
-        returnString = "You won! Your paper beat the computer's rock";
-    } else if (rpsInt === 2 && compChoice === 3) {
-        returnString = "You lost! Your paper was cut by the computer's scissors";
-    } else if (rpsInt === 3 && compChoice === 1) {
-        returnString = "You lost! Your scissors were smashed by the computer's rock";
-    } else if (rpsInt === 3 && compChoice === 2) {
-        returnString = "You won! Your scissors cut the computer's paper";
-    }
-
-    return returnString;
+    game(playerChoice, computerSelection());    
 }
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
+function computerSelection() {
+    return Math.floor(Math.random() * 3) + 1;
+}
 
-    while (playerScore + computerScore < 5) {
-        let winString = playRound(getComputerChoice(), getPlayerChoice());
-        if (winString.includes("tie")) {
-            // No points awarded to either side for a tie
-        } else if (winString.includes("lost")) {
+let info = document.getElementById('info');
+let scoreInfo = document.getElementById('score-info');
+let playerScoreInfo = document.getElementById('player');
+let computerScoreInfo = document.getElementById('computer');
+
+function game(pChoice, cChoice) {
+    if (!gameOver) {
+        let choiceStrings = [pChoice, cChoice];
+        choiceStrings.forEach(function(choice, index) {
+            switch(choice) {
+                case 1:
+                    choiceStrings[index] = "rock";
+                    break;
+                case 2:
+                    choiceStrings[index] = "paper";
+                    break;
+                case 3:
+                    choiceStrings[index] = "scissors";
+                    break;
+            }
+        });
+
+        info.textContent = `You played ${choiceStrings[0]}. Computer played ${choiceStrings[1]}.`;
+
+        if (pChoice === cChoice) {
+            scoreInfo.textContent = "Tie. No points assigned to either side.";
+        } else if (pChoice === cChoice - 1 || (pChoice === 3 && cChoice === 1)) {
+            scoreInfo.textContent = "Computer awarded a point.";
             computerScore++;
-        } else if (winString.includes("won")) {
+        } else {
+            scoreInfo.textContent = "Player awarded a point.";
             playerScore++;
         }
 
-        console.log(winString);
-    }
+        playerScoreInfo.textContent = `Player Score: ${playerScore}`;
+        computerScoreInfo.textContent = `Computer Score: ${computerScore}`;
 
-    if (playerScore > computerScore) {
-        console.log("You won the game!");
-    } else {
-        console.log("The computer won :(");
+        if (playerScore === 5 || computerScore === 5) {
+            gameOver = true;
+            if (playerScore === 5) {
+                scoreInfo.textContent += " Game Over. Player wins!";
+            } else {
+                scoreInfo.textContent += " Game Over. Computer wins!";
+            }
+        }
     }
 }
 
-game();
